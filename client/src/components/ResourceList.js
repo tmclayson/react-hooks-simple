27970b4ff0497de58240c5581a6fe45f6aa4dbda
whitebloodcell <tmclayson@gmail.com>
2019-02-07 07:15:07 +0100

    used componentDidUpdate with check on prevProps to stop endless requests

--- client/src/components/ResourceList.js
+++ client/src/components/ResourceList.js
@@ -3,9 +3,20 @@ import axios from 'axios';
 
 export default class ResourceList extends Component {
   state = { resources: [] };
+  // componentDidMount is not called on each re-render
   componentDidMount = async () => {
     const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
     this.setState({ resources: response.data });
+  };
+
+  // is called on every parent re-redner and every change of state
+  componentDidUpdate = async (prevProps, prevState) => {
+    if (prevProps.resource !== this.props.resource) {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/${this.props.resource}`
+      );
+      this.setState({ resources: response.data });
+    }
   };
 
   render() {
